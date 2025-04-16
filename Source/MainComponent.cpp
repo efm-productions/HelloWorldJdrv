11ef33ae732c798db037f7f20db6ea068dd2c5e1
Etienne Mauge <efm_productions@yahoo.com>
2025-04-16 16:37:44 -0400

    Changed windows text

--- Source/MainComponent.cpp
+++ Source/MainComponent.cpp
@@ -18,7 +18,7 @@ void MainComponent::paint (juce::Graphics& g)
 
     g.setFont (juce::FontOptions (16.0f));
     g.setColour (juce::Colours::white);
-    g.drawText ("Hello World!", getLocalBounds(), juce::Justification::centred, true);
+    g.drawText ("Hello new PFM C++ students!", getLocalBounds(), juce::Justification::centred, true);
 }
 
 void MainComponent::resized()
