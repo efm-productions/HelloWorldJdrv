8a7b396a529cc2a5fc9c1e3f8354cc4cb659306b
Etienne Mauge <efm_productions@yahoo.com>
2025-04-25 00:15:17 -0400

    Defined a Car and CarSeat user defined Type

--- Source/MainComponent.h
+++ Source/MainComponent.h
@@ -2,7 +2,30 @@
 
 #include <JuceHeader.h>
 
-//==============================================================================
+//EFM === Code Starts here
+
+//define a User Defined Type for ch.2 Pt.05
+struct Car
+{
+    struct CarSeat
+    {
+        bool seatIsLeather;
+    };
+    int numberOfWheels;
+    bool convertible;
+
+    CarSeat driversSeat;
+    CarSeat navigatorsSeat;
+
+    bool switchSeats(CarSeat oldSeat, CarSeat newSeat);
+
+    void accelerate(float howFarToPushThePedal);
+
+};
+
+//EFM === Code Ends here
+
+    //==============================================================================
 /*
     This component lives inside our window, and this is where you should put all
     your controls and content.
